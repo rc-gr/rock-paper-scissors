baa985d247b35a50790cc863c6b05d0377b1fa5d
RCGR <138417636+rc-gr@users.noreply.github.com>
2023-07-26 22:33:39 +0800

    Fix random roll in getComputerChoice

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // get a random move from the computer
+function getComputerChoice() {
   // init computer's choice to nothing
-  // init a random roll between 0 to 2
+  let computerChoice = "";
+  // init a random roll between 0 to 3
+  // clamp the result of the roll
+  let roll = Math.floor(Math.random() * 3);
   // if the roll is 0
     // set computer's choice to Rock
   // else if the roll is 1
@@ -9,3 +13,4 @@
     // set computer's choice to Scissors
   // else
     // roll has failed
+}
