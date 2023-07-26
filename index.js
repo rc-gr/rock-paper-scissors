91e5bbf3f8cf7082d252bed70f51eccbeb7e244d
RCGR <138417636+rc-gr@users.noreply.github.com>
2023-07-26 22:22:11 +0800

    Add pseudocode for getComputerChoice

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+// get a random move from the computer
+  // init computer's choice to nothing
+  // init a random roll between 0 to 2
+  // if the roll is 0
+    // set computer's choice to Rock
+  // else if the roll is 1
+    // set computer's choice to Paper
+  // else if the roll is 2
+    // set computer's choice to Scissors
+  // else
+    // roll has failed
